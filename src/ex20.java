1c5f19cebabbd3be71441a67f24dc6db059aa6df
GitdoPedro <emailpedrolima@gmail.com>
2023-02-14 14:21:42 -0300

    ex 20

---
+++ src/ex20.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex20 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(5,10,15);
+        List<Integer> numeros2 = Arrays.asList(7,8,19,20);
+        List<Integer> numerosFinal = new ArrayList<>();
+
+
+
+
+
+
+
+
+    }
+}
