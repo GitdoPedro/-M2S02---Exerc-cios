4d86675350efdb47f6c998a7807995e2fc30bb3a
GitdoPedro <emailpedrolima@gmail.com>
2023-02-25 13:21:46 -0300

    ex21

---
+++ src/ex21.java
@@ -0,0 +1,40 @@
+public class ex21 {
+
+    private String nomeSegurado;
+    private int idade;
+    private double valorPremio;
+
+    void imprimir(){
+        System.out.println("O " + nomeSegurado + " tem " + idade + " anos e possui valor premio de R$" + valorPremio );
+    }
+
+    void calcularPremioApolice(){
+        if (idade >= 18 && idade < 25){
+            valorPremio += (valorPremio*20)/100;
+        }if (idade >25 && idade <= 36){
+            valorPremio+=(valorPremio*15)/100;
+        }else{
+            valorPremio+=(valorPremio*10)/100;
+        }
+    }
+
+    void oferecerDesconto(String cidade){
+        switch (cidade){
+            case "Curitiba":
+                valorPremio+=valorPremio*0.8;
+                break;
+            case "Rio de Janeiro":
+                valorPremio+=valorPremio*0.85;
+                break;
+            case "São Paulo":
+                valorPremio+=valorPremio*0.9;
+                break;
+            case "Belo Horizonte":
+                valorPremio+=valorPremio*0.95;
+                break;
+            default:
+                break;
+        }
+
+    }
+}
